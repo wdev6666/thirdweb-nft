7df4dfdb334cf51fa631967e064a9a6803b9a661
Naresh <wdev6666@gmail.com>
2022-11-06 19:03:56 +0530

    Final

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,22 +1,34 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
-import "@thirdweb-dev/contracts/base/ERC1155SignatureMint.sol";
+import "@thirdweb-dev/contracts/base/ERC721Base.sol";
+import "@thirdweb-dev/contracts/extension/PermissionsEnumerable.sol";
 
-contract Contract is ERC1155SignatureMint {
+contract Contract is ERC721Base, PermissionsEnumerable {
+
+    mapping(uint => string) public nfts;
+    bytes32 public constant IDENTITY_ROLE = keccak256("IDENTITY_ROLE");
+
+    function setNickname(uint _tokenId, string calldata _identity) public onlyRole(IDENTITY_ROLE) {
+        nfts[_tokenId] = _identity;
+    }
+
     constructor(
         string memory _name,
         string memory _symbol,
         address _royaltyRecipient,
-        uint128 _royaltyBps,
-        address _primarySaleRecipient
+        uint128 _royaltyBps
     )
-        ERC1155SignatureMint(
+        ERC721Base(
             _name,
             _symbol,
             _royaltyRecipient,
-            _royaltyBps,
-            _primarySaleRecipient
+            _royaltyBps
         )
-    {}
+    {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(IDENTITY_ROLE, msg.sender);
+    }
+
+
 }
